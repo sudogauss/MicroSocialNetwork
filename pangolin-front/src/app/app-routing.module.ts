a8093cd3814cd2403099dd6a1b027540e4c7b63b
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2021-04-05 13:57:39 +0200

    basic front with routing

--- pangolin-front/src/app/app-routing.module.ts
+++ pangolin-front/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FriendsComponent } from './friends/friends.component';
+import { HomeComponent } from './home/home.component';
+import { InfosComponent } from './infos/infos.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'infos', component: InfosComponent},
+  {path: 'friends', component: FriendsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pangolin-front/src/app/app.module.ts
+++ pangolin-front/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { InfosComponent } from './infos/infos.component';
+import { FriendsComponent } from './friends/friends.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent,
+    InfosComponent,
+    FriendsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
